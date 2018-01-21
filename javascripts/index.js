d0e93b23c34516ed4b81e2f5f9914bea19cbdc81
Azumi Callanan <azucm@hotmail.com>
2018-01-21 17:32:17 +0000

    automatically backed up by learn

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,12 @@
-$(#yo)
+$(document).ready(function(){
+  $('#yo').append("Hey everybody!");
+});
+
+When the code gets longer, you can declare a function separately and insert only the function name.
+$(document).ready(function(){
+  heyEverybody();
+});
+
+function heyEverybody(){
+  $('#yo').append("Hey everybody!");
+}
